6066529710078f68ea01edacc9cf72bcdb64db54
Abhimanyu Gupta <abhgupta@expedia.com>
2015-07-05 13:46:35 +0530

    Adding ValueAssertionCallStub and its unit tests

---
+++ src/main/java/com/hotels/plunger/ValueAssertionCallStub.java
@@ -0,0 +1,104 @@
+package com.hotels.plunger;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import cascading.flow.FlowProcess;
+import cascading.operation.Function;
+import cascading.operation.ValueAssertion;
+import cascading.operation.ValueAssertionCall;
+import cascading.tuple.Fields;
+import cascading.tuple.FieldsResolverException;
+import cascading.tuple.Tuple;
+import cascading.tuple.TupleEntry;
+
+/**
+ * For stubbing {@link ValueAssertionCall} in {@link ValueAssertion} implementations. Primarily this class stores the
+ * context state, but is also used to deliver {@link TupleEntry TupleEntries} and collect output from the function.
+ * <p/>
+ * Make sure that you call {@link #nextOperateCall()} to advance to the next set of argument values}.
+ */
+public final class ValueAssertionCallStub<C> extends AbstractOperationCallStub<C> implements ValueAssertionCall<C> {
+
+  private final Iterator<TupleEntry> arguments;
+
+  private TupleEntry currentArguments;
+
+  private ValueAssertionCallStub(Fields argumentFields, Fields declaredFields, Iterable<TupleEntry> arguments) {
+    super(argumentFields, declaredFields);
+    this.arguments = arguments.iterator();
+  }
+
+  @Override
+  public TupleEntry getArguments() {
+    return currentArguments;
+  }
+
+  /** Advances to the next arguments value. */
+  public ValueAssertionCallStub<C> nextAssertionCall() {
+    currentArguments = arguments.next();
+    return this;
+  }
+
+  /** Processes the groups with the provided {@link Function}. */
+  public ValueAssertionCallStub<C> complete(FlowProcess<?> flowProcess, ValueAssertion<C> valueAssertionCall) {
+
+    valueAssertionCall.prepare(flowProcess, this);
+
+    while (arguments.hasNext()) {
+      valueAssertionCall.doAssert(flowProcess, nextAssertionCall());
+    }
+    valueAssertionCall.flush(flowProcess, this);
+    valueAssertionCall.cleanup(flowProcess, this);
+    return this;
+  }
+
+  static public class Builder<C> {
+    private final Fields fields;
+    private final List<TupleEntry> tuples = new ArrayList<TupleEntry>();
+    private Fields fieldMask;
+    private Fields outputFields;
+
+    Builder(Fields fields) {
+      if (fields == null) {
+        throw new IllegalArgumentException("fields == null");
+      }
+      this.fields = fields;
+      fieldMask = fields;
+    }
+
+    /** Specify the output fields when they are different to the nonGroupFields. */
+    public Builder<C> outputFields(Fields outputFields) {
+      if (outputFields == null) {
+        throw new IllegalArgumentException("outputFields == null");
+      }
+      this.outputFields = outputFields;
+      return this;
+    }
+
+    public Builder<C> addTuple(Object... values) {
+      values = FieldTypeValidator.validateValues(fieldMask, values);
+      TupleEntry newTuple = new TupleEntry(fields, Tuple.size(fields.size()));
+      newTuple.setTuple(fieldMask, new Tuple(values));
+      tuples.add(newTuple);
+      return this;
+    }
+
+    public Builder<C> withFields(Fields... fields) {
+      Fields fieldMask = Fields.merge(fields);
+      try {
+        this.fields.select(fieldMask);
+        this.fieldMask = fieldMask;
+      } catch (FieldsResolverException e) {
+        throw new IllegalArgumentException("selected fields must be contained in record fields: selected fields="
+            + fieldMask + ", source fields=" + this.fields);
+      }
+      return this;
+    }
+
+    public ValueAssertionCallStub<C> build() {
+      Fields newFields = outputFields != null ? outputFields : fields;
+      return new ValueAssertionCallStub<C>(fields, newFields, tuples);
+    }
+  }
+}
---
+++ src/test/java/com/hotels/plunger/ValueAssertionCallStubTest.java
@@ -0,0 +1,148 @@
+/**
+ * Copyright 2015 Expedia Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hotels.plunger;
+
+import static com.hotels.plunger.asserts.PlungerAssert.tupleEntry;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import cascading.flow.FlowProcess;
+import cascading.operation.Identity;
+import cascading.operation.assertion.AssertNotNull;
+import cascading.tuple.Fields;
+import cascading.tuple.Tuple;
+import cascading.tuple.TupleEntry;
+
+public class ValueAssertionCallStubTest {
+
+  private static final Fields FIELDS = new Fields("field");
+  private static final Fields OUTPUT = new Fields("output");
+
+  private ValueAssertionCallStub<String> stub;
+
+  @Test
+  public void next() {
+    stub = new ValueAssertionCallStub.Builder<String>(FIELDS).addTuple(1).addTuple(2).build();
+    assertThat(stub.getArguments(), is(nullValue()));
+
+    stub.nextAssertionCall();
+    assertThat(stub.getArguments(), is(new TupleEntry(FIELDS, new Tuple(1))));
+
+    stub.nextAssertionCall();
+    assertThat(stub.getArguments(), is(new TupleEntry(FIELDS, new Tuple(2))));
+  }
+
+  @Test
+  public void fields() {
+    stub = new ValueAssertionCallStub.Builder<String>(FIELDS).build();
+    assertThat(stub.getArgumentFields(), is(FIELDS));
+    assertThat(stub.getDeclaredFields(), is(FIELDS));
+  }
+
+  @Test
+  public void argumentsFieldsIgnoreOutputFields() {
+    stub = new ValueAssertionCallStub.Builder<String>(FIELDS).outputFields(OUTPUT).build();
+    assertThat(stub.getArgumentFields(), is(FIELDS));
+    assertThat(stub.getDeclaredFields(), is(OUTPUT));
+  }
+
+  @Test
+  public void context() {
+    stub = new ValueAssertionCallStub.Builder<String>(FIELDS).addTuple(1).addTuple(2).build();
+    assertThat(stub.getContext(), is(nullValue()));
+
+    stub.setContext("VALUE");
+    assertThat(stub.getContext(), is("VALUE"));
+
+    stub.nextAssertionCall();
+    assertThat(stub.getContext(), is("VALUE"));
+  }
+
+  @Test
+  public void collectTupleEntry() {
+    stub = new ValueAssertionCallStub.Builder<String>(FIELDS).build();
+    assertThat(stub.result().asTupleEntryList().isEmpty(), is(true));
+
+    stub.getOutputCollector().add(new TupleEntry(FIELDS, new Tuple(1)));
+    assertThat(stub.result().asTupleEntryList().size(), is(1));
+    assertThat(stub.result().asTupleEntryList().get(0), is(new TupleEntry(FIELDS, new Tuple(1))));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void collectIrregularTupleEntry() {
+    stub = new ValueAssertionCallStub.Builder<String>(FIELDS).build();
+    assertThat(stub.result().asTupleEntryList().isEmpty(), is(true));
+
+    stub.getOutputCollector().add(new TupleEntry(new Fields("X", String.class), new Tuple(1)));
+  }
+
+  @Test
+  public void collectTuple() {
+    stub = new ValueAssertionCallStub.Builder<String>(FIELDS).build();
+    assertThat(stub.result().asTupleEntryList().isEmpty(), is(true));
+
+    stub.getOutputCollector().add(new Tuple(1));
+    assertThat(stub.result().asTupleEntryList().size(), is(1));
+    assertThat(stub.result().asTupleEntryList().get(0), is(new TupleEntry(FIELDS, new Tuple(1))));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void collectIrregularTuple() {
+    stub = new ValueAssertionCallStub.Builder<String>(FIELDS).build();
+    assertThat(stub.result().asTupleEntryList().isEmpty(), is(true));
+
+    stub.getOutputCollector().add(new Tuple(1, 2));
+  }
+
+  // TODO: test withFields()
+
+  @SuppressWarnings({ "rawtypes", "unchecked" })
+  @Test
+  public void complete() {
+    List<TupleEntry> actual = new FunctionCallStub.Builder(FIELDS)
+        .addTuple("a")
+        .addTuple("b")
+        .build()
+        .complete(mock(FlowProcess.class), new Identity())
+        .result()
+        .asTupleEntryList();
+
+    assertThat(actual.size(), is(2));
+    assertThat(actual.get(0), tupleEntry(FIELDS, "a"));
+    assertThat(actual.get(1), tupleEntry(FIELDS, "b"));
+  }
+
+  @Test
+  public void completeDifferentOutputFields() {
+    @SuppressWarnings("unchecked")
+    List<TupleEntry> actual = new ValueAssertionCallStub.Builder<Void>(FIELDS)
+        .outputFields(OUTPUT)
+        .addTuple("a")
+        .addTuple("b")
+        .build()
+        .complete(mock(FlowProcess.class), new AssertNotNull())
+        .result()
+        .asTupleEntryList();
+
+    assertThat(actual.size(), is(0));
+  }
+}
